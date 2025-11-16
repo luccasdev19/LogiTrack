7f3f197ad203be0b0d3c3bf2043f43ce3cf03173
Lucas Rodrigues <lucasrodrigues.oliveira0809@gmail.com>
2025-11-15 23:27:07 -0300

    production

---
+++ main.js
@@ -0,0 +1,12 @@
+function showPage(pageId) {
+    const pages = document.querySelectorAll('.page');
+    pages.forEach(page => page.classList.remove('active'));
+    
+    const selectedPage = document.getElementById(pageId);
+    if (selectedPage) {
+        selectedPage.classList.add('active');
+    }
+}
+document.addEventListener('DOMContentLoaded', () => {
+    showPage('home');
+});
